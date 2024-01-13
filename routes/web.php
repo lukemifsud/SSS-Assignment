ba2f756f24a454b418c41f9c00f905a7c66982cf
lukemifsud <luke200423@gmail.com>
2024-01-13 15:43:53 +0000

    basic routing implementations

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route for cars
+Route::get('/cars', function()
+{
+    return "<h1>All Cars</h1>";
+});
+
+Route::get('/cars/create', function()
+{
+    return "<h1>Add new car</h1>";
+});
+
+//returns all Cars
+Route::get('/cars/all', function()
+{
+    return App\Models\Cars::all();
+});
