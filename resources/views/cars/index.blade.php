a9f9c5bd944dd2ac8b2a1f739fe9f3ea9818cf6f
lukemifsud <luke200423@gmail.com>
2024-01-14 13:01:49 +0000

    Included 'includes' and 'yields' in index layout

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 
+@section('content')
 <h1> All Cars <h1>
-
 <a href="{{ route('cars.add') }}">Add Car</a>
 <a href="{{ route('cars.show') }}">Show all cars</a>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,7 +18,7 @@
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container">
         <a class="navbar-brand text-uppercase" href="index.html">            
-            <strong>Cars</strong> App
+            <strong>Contact</strong> App
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -27,13 +27,14 @@
         <!-- /.navbar-header -->
         <div class="collapse navbar-collapse" id="navbar-toggler">
           <ul class="navbar-nav">
-            <li class="nav-item"><a href="#" class="nav-link">Manufacturers</a></li>
-            <li class="nav-item active"><a href="#" class="nav-link">Cars</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Companies</a></li>
+            <li class="nav-item active"><a href="#" class="nav-link">Contacts</a></li>
           </ul>
         </div>
       </div>
     </nav>
 
+    @yield('content')
 
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
