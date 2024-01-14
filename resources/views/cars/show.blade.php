9e437afe37c0c4696c61a98c15f5cf580c5b58de
lukemifsud <luke200423@gmail.com>
2024-01-14 15:40:00 +0000

    added the view car details page + added cancel button fucntionality

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -1,4 +1,60 @@
-<h1>Car details</h1>
-<div>{{ $cars }}</div>
+@extends('layouts.main')
 
-<a href="{{ route('cars.index') }}">Back to show all cars</a>
+@section('content')
+<main class="py-5">
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col-md-8">
+            <div class="card">
+              <div class="card-header card-title">
+                <strong>Car Details</strong>
+              </div>           
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group row">
+                      <label for="first_name" class="col-md-3 col-form-label">Model</label>
+                      <div class="col-md-9">
+                        <p class="form-control-plaintext text-muted">{{ $cars->model }}</p>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <label for="last_name" class="col-md-3 col-form-label">Year</label>
+                      <div class="col-md-9">
+                        <p class="form-control-plaintext text-muted">{{ $cars->year }}</p>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <label for="email" class="col-md-3 col-form-label">Salesperson Email</label>
+                      <div class="col-md-9">
+                        <p class="form-control-plaintext text-muted">{{ $cars->salesperson_email }}</p>
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <label for="phone" class="col-md-3 col-form-label">Manufacturer</label>
+                      <div class="col-md-9">
+                        <p class="form-control-plaintext text-muted">{{ $cars->manufacturers->name }}</p>
+                      </div>
+                    </div>
+
+                   
+                    <hr>
+                    <div class="form-group row mb-0">
+                      <div class="col-md-9 offset-md-3">
+                          <a href="#" class="btn btn-info">Edit</a>
+                          <a href="#" class="btn btn-outline-danger">Delete</a>
+                          <a href="{{ route('cars.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+@endsection
