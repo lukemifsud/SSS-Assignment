9efb86f5cfee61d06e28cd52da59a1b9ba87b92f
lukemifsud <luke200423@gmail.com>
2024-01-15 18:37:52 +0000

    Final fixes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,7 +19,7 @@
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container">
         <a class="navbar-brand text-uppercase" href="index.html">            
-            <strong>Contact</strong> App
+            <strong>Carozza</strong> App
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -28,8 +28,8 @@
         <!-- /.navbar-header -->
         <div class="collapse navbar-collapse" id="navbar-toggler">
           <ul class="navbar-nav">
-            <li class="nav-item"><a href="#" class="nav-link">Companies</a></li>
-            <li class="nav-item active"><a href="#" class="nav-link">Contacts</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Cars</a></li>
+            <li class="nav-item active"><a href="#" class="nav-link">Manufacturers</a></li>
           </ul>
         </div>
       </div>
